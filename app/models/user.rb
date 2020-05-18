0634aa2df0586e31eb38c9297dad6b3fad64f823
masataka <hanayoriinko@gmail.com>
2020-05-18 19:26:20 +0900

    課題３

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :books, dependent: :destroy
-  attachment :profile_image
-
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: { maximum:50 }
+
+  has_many :favorites, dependent: :destroy
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  attachment :profile_image
+  def already_favorited?(book)
+  	self.favorites.exists?(book_id: book.id)
+  end
+
 end
